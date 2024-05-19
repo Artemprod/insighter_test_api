998abeac0da24d8a1b95fd082e9796086b95a38f
artemprod <artemprod1@gmail.com>
2024-05-19 16:29:35 +0400

    first commit

--- routers/dev_router.py
+++ routers/dev_router.py
@@ -1,14 +1,11 @@
-from pathlib import Path
-
-from fastapi import HTTPException, APIRouter
-from starlette.requests import Request
+from fastapi import HTTPException, APIRouter, Request, Path
 from app.publishers.publisher import Publisher
 
+# Создание экземпляра роутера
 dev_router = APIRouter(
     prefix="/development",
     tags=["Endpoints for development"],
 )
-
 publisher = Publisher("nats://demo.nats.io:4222")
 
 @dev_router.get("/assistant/get_all")
@@ -28,12 +25,10 @@ assistant_db = {
     1: {"id": 1, "name": "Assistant One", "role": "Helper"},
     2: {"id": 2, "name": "Assistant Two", "role": "Supporter"}
 }
-
 transcribed_text_db = {
     1: {"id": 1, "text": "This is a transcribed text."},
     2: {"id": 2, "text": "Another transcribed text."}
 }
-
 summary_text_db = {
     1: {"id": 1, "summary": "This is a summary of the text."},
     2: {"id": 2, "summary": "This is another summary."}
@@ -70,6 +65,7 @@ async def get_summary_text(id: int = Path(..., description="The ID of the summar
             raise HTTPException(status_code=404, detail="Summary text not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
 @dev_router.post("/start/youtube")
 async def start_youtube(request: Request):
     try:
